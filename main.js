6f09c62f65f4939811be58048096dbe1f3267e17
student1432 <84581003+student1432@users.noreply.github.com>
2021-05-21 19:53:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function function1(){
+ document.getElementById("img").src="https://bstatic.com/xdata/images/xphoto/1182x887/82877075.jpg?k=db9e00135b7b8f038aad88a7676235667ca249a5eed997a499677812fa332833&o=?size=S";
+}
